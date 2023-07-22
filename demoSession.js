241c2c2fcef6db792e5447641a89437bb4ad03a7
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-07-22 12:27:36 +0100

    add new comment

--- demoSession.js
+++ demoSession.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('this is a test request for Beko application')
+    res.send('this is a test request for Beko new application')
 })
 
 const port = 3010;
