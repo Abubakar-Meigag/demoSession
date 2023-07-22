5cd96d1ce5fc84d0a9cd96d0ab3cde511bc7ba5d
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-07-22 11:36:37 +0100

    frist comment

--- demoSession.js
+++ demoSession.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('this is a test request for my application')
+})
 
 const port = 3010;
 app.listen(port, () => console.log(`this my server listen to ${port}`));
